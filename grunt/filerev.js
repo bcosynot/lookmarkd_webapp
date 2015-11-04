ebdf8b589b60ede564775ec5c1806f76eb5c5beb
Vivek <vr3690@gmail.com>
2015-11-04 10:04:14 -0600

    Fixes #3
    The needed styles were not being loaded properly.

--- grunt/filerev.js
+++ grunt/filerev.js
@@ -3,8 +3,8 @@ module.exports = {
     dist: {
         src: [
             '<%= paths.dist %>/img/**/*.{jpg,jpeg,gif,png,webp}',
-            '<%= paths.dist %>/styles/main.css',
-            '<%= paths.dist %>/scripts/main.js'
+            '<%= paths.dist %>/styles/*.css',
+            '<%= paths.dist %>/scripts/*.js'
         ]
     }
 };
