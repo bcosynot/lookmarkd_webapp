1f85c6a11cb839d396d6b08497a986f3a0dfa335
Vivek <vr3690@gmail.com>
2015-12-09 10:49:35 -0600

    Updated: Loading Instagram stats via ajax. See #21

--- app/Resources/public/scripts/controllers/dashboard/dashboard-influencer.js
+++ app/Resources/public/scripts/controllers/dashboard/dashboard-influencer.js
@@ -24,5 +24,25 @@ define(function(require, exports) {
 								});
 							}
 						});
+		
+		$.getJSON($('#followerCount').attr('data-url'),{},function(data){
+			if(typeof data !== undefined) {
+				$('#followerCount > div > h3').text(data.followerCount+' followers');
+				$('#followerCount p').text('You have '+data.followerCount+' follower(s) on Instagram.');
+			}
+		});
+		
+		$.getJSON($('#mediaCount').attr('data-url'),{},function(data){
+			if(typeof data !== undefined) {
+				$('#mediaCount > div > h3').text(data.mediaCount+' post(s)');
+				$('#mediaCount p').text('You have '+data.mediaCount+' post(s) on Instagram.');
+				
+				$('#likesCount > div > h3').text(data.likesCount+' like(s)');
+				$('#likesCount p').text('You have '+data.likesCount+' like(s) across '+data.mediaCount+' post(s) on Instagram.');
+				
+				$('#commentsCount > div > h3').text(data.commentsCount+' comment(s)');
+				$('#commentsCount p').text('You have '+data.commentsCount+' comments(s) across '+data.mediaCount+' post(s) on Instagram.');
+			}
+		});
 	};
 });
