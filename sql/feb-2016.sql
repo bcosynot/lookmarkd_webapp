c80b8772de5f292810cde2105bc1b6b32099b0ef
Vivek <vr3690@gmail.com>
2016-02-01 07:40:27 -0600

    Added: instagram details can be null for brand user

---
+++ sql/feb-2016.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `fos_user` CHANGE `instagram_id` `instagram_id` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL;
+
+ALTER TABLE `fos_user` CHANGE `instagram_access_token` `instagram_access_token` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL;
