7b7c1c522a5dc941ab904a54170446f65ed186f1
Vivek <vr3690@gmail.com>
2016-02-11 21:24:37 -0600

    Fixed: Issues with styling
    1. Create campaign page was blank on prod environment
    2. Removed unused imports in controller.

--- src/AppBundle/Controller/CampaignController.php
+++ src/AppBundle/Controller/CampaignController.php
@@ -4,9 +4,6 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use MetzWeb\Instagram\Instagram;
-use AppBundle\Entity\User;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
 class CampaignController extends Controller
 {
