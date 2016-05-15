b37517157fee75d87c92c90b24b924d56839aeef
Vivek <vr3690@gmail.com>
2016-05-15 13:51:00 -0500

    Wrapping up campaign creation.

---
+++ src/AppBundle/Core/Service/CampaignServiceInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Vivek
+ * Date: 14-05-2016
+ * Time: 09:48 PM
+ */
+
+namespace AppBundle\Core\Service;
+
+
+interface CampaignServiceInterface
+{
+
+}
